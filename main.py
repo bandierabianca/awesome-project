5857da1b1a62fc0fe3afe764ea7a7358f2072e58
Elisa Costagli <costagli.elisa@gmail.com>
2025-08-03 18:42:18 +0200

    commit da VS Code

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from enum import Enum
 
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     uno = "uno"
     due = "due"
     tre = "tre"
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None= None
 
 
 app = FastAPI()
@@ -18,7 +24,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/")
+@app.get("/items_query/")
 async def read_item_query(skip:int =0, limit: int = 10):
     return fake_items_db[skip + limit]
 
@@ -26,7 +32,6 @@ async def read_item_query(skip:int =0, limit: int = 10):
 @app.get("/items/{item_id}")
 async def read_item(item_id:int):
     return {"item_id": item_id}
-
 
 
 @app.get("/get_model/{model_name}")
@@ -38,3 +43,13 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "E' due!"}
     
     return {"model_name": model_name, "message": "Mmmmm secondo me è 3!"}
+
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
