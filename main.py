4ab47fcaf5215bddcfc4a44d1ef2870aa5b6cde0
Elisa Costagli <costagli.elisa@gmail.com>
2025-08-03 18:58:24 +0200

    altro commit

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ async def get_model(model_name: ModelName):
 async def create_item(item: Item):
     item_dict = item.dict()
     if item.tax is not None:
-        price_with_tax = item.price + item.tax + 0
+        price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
